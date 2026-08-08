476ea9823d2dd30aef9148d1f2d71920ec1cad72
Russ Olson <rj.olson42@gmail.com>
2026-08-07 21:28:00 -0500

    started adding method definitions to BattleGrid.cpp and completed Unit.h

--- dev/Portfolio Project/Portfolio Project/cpp Files/BattleGrid.cpp
+++ dev/Portfolio Project/Portfolio Project/cpp Files/BattleGrid.cpp
@@ -42,3 +42,49 @@ int BattleGrid::GetHeight() const {
 }
 
 //class methods
+bool BattleGrid::isValidPosition(int x, int y) const {
+
+	//returning true if position is within bounds
+	return (x >= 0 && x < width && y >= 0 && y < height);
+}
+
+bool BattleGrid::placeUnit(Unit* unit, int targetX, int targetY) {
+
+    //checking if position is in bounds
+    if (!isValidPosition(targetX, targetY)) {
+
+        //alerting user that the position is out of bounds
+        std::cout << "[Grid Error] Target position (" << targetX << ", " << targetY << ") is out of bounds!\n";
+        return false;
+    }
+
+    //checking if position is occipied
+    if (grid[targetY][targetX].IsOccupied()) {
+
+        //alerting user that the position is occupied
+        std::cout << "[Grid Error] Tile (" << targetX << ", " << targetY << ") is already occupied!\n";
+        return false;
+    }
+
+    //storing units current position
+    std::pair<int, int> currentPos = unit->GetPosition();
+
+    //checking if unit is being moved from another valid position
+    if (isValidPosition(currentPos.first, currentPos.second)) {
+
+        //checking if unit is actually at the position being moved from
+        if (grid[currentPos.second][currentPos.first].GetOccupant() == unit) {
+
+            //removing unit from the Tile
+            grid[currentPos.second][currentPos.first].RemoveOccupant();
+        }
+    }
+
+    //adding unit to the Tile
+    grid[targetY][targetX].SetOccupant(unit);
+
+    //setting units new postion
+    unit->SetPosition(targetX, targetY);
+
+    return true;
+}
