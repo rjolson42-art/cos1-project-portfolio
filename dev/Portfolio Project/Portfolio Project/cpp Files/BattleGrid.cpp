b16970a6945020906080c53defe66604c2d2f8dc
Russ Olson <rj.olson42@gmail.com>
2026-08-07 20:35:23 -0500

    implemented definitions for the Tile class in Tile.cpp

--- dev/Portfolio Project/Portfolio Project/cpp Files/BattleGrid.cpp
+++ dev/Portfolio Project/Portfolio Project/cpp Files/BattleGrid.cpp
@@ -8,5 +8,37 @@ BattleGrid::BattleGrid() : width(10), height(10) {
 	//resizing grid
 	grid.resize(height, std::vector<Tile>(width));
 
-	//adding coordinates to each Tile of grid
+	//adding Tiles to grid
+	for (int r = 0; r < height; ++r) {
+		for (int c = 0; c < width; ++c) {
+			grid[r][c] = Tile(c, r);
+		}
+	}
 }
+
+//overload constructor
+BattleGrid::BattleGrid(int gridWidth, int gridHeight) : width(gridWidth), height(gridHeight) {
+
+	//resizing grid
+	grid.resize(height, std::vector<Tile>(width));
+
+	//adding Tiles to grid
+	for (int r = 0; r < height; ++r) {
+		for (int c = 0; c < width; ++c) {
+			grid[r][c] = Tile(c, r);
+		}
+	}
+}
+
+//getters
+int BattleGrid::GetWidth() const {
+
+	return width;
+}
+
+int BattleGrid::GetHeight() const {
+
+	return height;
+}
+
+//class methods
--- dev/Portfolio Project/Portfolio Project/cpp Files/Tile.cpp
+++ dev/Portfolio Project/Portfolio Project/cpp Files/Tile.cpp
@@ -5,5 +5,46 @@ Tile::Tile() : x(0), y(0), traversable(true), occupant(nullptr) {}
 
 //overload constructor
 Tile::Tile(int tileX, int tileY, bool isTraversable)
-    : x(tileX), y(tileY), traversable(isTraversable), occupant(nullptr) {
+    : x(tileX), y(tileY), traversable(isTraversable), occupant(nullptr) {}
+
+//getters
+int Tile::GetX() const { 
+    
+    return x; 
+}
+
+int Tile::GetY() const { 
+    
+    return y; 
+}
+
+//methods
+bool Tile::IsTraversable() const {
+
+    //returning true if Tile is both traversable and unoccupied
+    return traversable && occupant == nullptr;
+}
+
+bool Tile::IsOccupied() const {
+
+    //returning true if Tile is unoccupied
+    return occupant != nullptr;
+}
+
+Unit* Tile::GetOccupant() const {
+
+    //returning occupant
+    return occupant;
+}
+
+void Tile::SetOccupant(Unit* unit) {
+
+    //setting occupant
+    occupant = unit;
+}
+
+void Tile::RemoveOccupant() {
+
+    //setting occupant to nullptr
+    occupant = nullptr;
 }
